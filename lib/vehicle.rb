ebd2a7013c831accecec13409ad8ecaa570ea8fd
nrymarz <nrymarz@gmail.com>
2020-10-28 17:38:16 -0500

    commit tree 9e4c7471acd417a84d158c5031abdbcec050834a

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,13 @@
 class Vehicle
-
+    def initialize(size,number)
+        @wheel_size = size
+        @wheel_number = number
+    end
+    attr_accessor :wheel_number, :wheel_size
+    def go
+        "vrrrrrrrooom!"
+    end
+    def fill_up_tank
+        'filling up!'
+    end
 end
